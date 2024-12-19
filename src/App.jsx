6e44c9456b855aa1231c045d5e5adce429130f7e
SabeeJeedorh <sabeeajeedorh@gmail.com>
2024-12-18 19:55:03 -0800

    Star-App

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,7 @@ const lists = [
     name: 'Dial D for Deadman',
     rating: 4,
   },
-  {
-    id: crypto.randomUUID(),
-    name: 'Words And Things',
-    rating: 0,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'The Galaxy, And The Ground Within',
-    rating: 5,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'Setemi the Instructor',
-    rating: 1,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'Jerry the Principal',
-    rating: 4,
-  },
+
 ];
 
 function App() {
